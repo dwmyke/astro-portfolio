3310e7531ec6f34e5e90e48ef1fff907817d25a5
Mikel-Jon West <dwmyke@gmail.com>
2023-12-05 16:14:37 -0600

    removed unwanted files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mikewest.site'
+    site: 'https://mikewest.site',
+    integrations: [AstroPWA()]
 });
