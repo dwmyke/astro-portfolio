8161dc8729027ae226f2b219119b4d13e25661a5
Mikel-Jon West <dwmyke@gmail.com>
2024-04-02 15:53:56 -0500

    Added netlify adapter for edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import db from "@astrojs/db";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poetic-blancmange-8263cf.netlify.app',
-  integrations: [db()]
+  integrations: [db()],
+  output: "server",
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
