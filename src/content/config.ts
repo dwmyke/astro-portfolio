e578d69293d6c332ce2c25bd37a8b0d62ce5cd99
Mikel-Jon West <dwmyke@gmail.com>
2024-07-10 17:00:56 -0500

    removed slug astro file, fixed seo

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,21 +2,22 @@
 import { z, defineCollection } from 'astro:content';
 
 // 2. Define a `type` and `schema` for each collection
-const projectCollection = defineCollection({
-  type: 'content', // v2.5.0 and later
+const projectsCollection = defineCollection({
+  type: 'data', // v2.5.0 and later
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    coverImage: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    releaseDate: z.date(),
     categories: z.array(z.string()),
-    tags: z.array(z.string()),
-    image: z.object({
-        src: z.string(),
-        alt: z.string(),
-      }),
-  }),
+    tags: z.array(z.string())
+  })
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-  'projects': projectCollection,
+  'projects': projectsCollection,
 };
